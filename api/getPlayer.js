417b5a800c70c95d35d99035f4c59d6a77e14d85
tinyheros <tinyherosgame@gmail.com>
2024-05-31 02:16:17 -0700

    update

--- api/getPlayer.js
+++ api/getPlayer.js
@@ -4,15 +4,8 @@ import { getPlayer, updatePlayer } from '../firebase';
 const cors = Cors();
 
 export default cors(async function handler(req, res) {
-    let response;
-    try {
-const response = await getPlayer(req.body)
-        res.status(200).json({
-            response
-        });
-    } catch (error) {
-        res.status(500).json({
-            response
-        });
-    }
+    const response = await getPlayer(req.body)
+    res.status(200).json({
+        response
+    });
 });
